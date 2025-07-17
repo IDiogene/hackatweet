f5f3f2adc968c3eecf450e31dc733b381f1e66b4
canadas hugo <canadashcb@gmail.com>
2025-07-17 12:10:41 +0200

    resolution bug front

--- front/hackatweet_front/pages/_app.js
+++ front/hackatweet_front/pages/_app.js
@@ -1,34 +1,39 @@
-import "../styles/globals.css";
-import Head from "next/head";
+import '../styles/globals.css';
+import Head from 'next/head';
 
-// import { Provider } from 'react-redux';
-// import { configureStore } from '@reduxjs/toolkit';
-// import counter from '../reducers/counter';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import storage from 'redux-persist/lib/storage';
-// import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-// const reducers = combineReducers({ counter });
-// const persistConfig = { key: 'applicationName', storage };
-// const store = configureStore({
-// reducer: persistReducer(persistConfig, reducers),
-// middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
-// });
-// const persistor = persistStore(store);
+import { Provider } from 'react-redux';
+
+import counter from '../reducers/user';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
+const reducers = combineReducers({ counter });
+const persistConfig = { key: 'applicationName', storage };
+
+const store = configureStore({
+reducer: persistReducer(persistConfig, reducers),
+middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+const persistor = persistStore(store);
 
 // englober le composant avec :
 
+
+
+
 function App({ Component, pageProps }) {
   return (
-    // <Provider store={store}>
-    //   <PersistGate persistor={persistor}>
-    <Head>
-      <title>Next.js App</title>
-    </Head>
-    // <Component {...pageProps} />
-    //   </PersistGate>
-    // </Provider>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <Head>
+        <title>Next.js App</title>
+      </Head>
+      <Component {...pageProps} />
+  </PersistGate>
+  </Provider>
   );
 }
 
