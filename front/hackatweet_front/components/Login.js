1821938877534fb7a7358524f70d16c80e92ff14
canadas hugo <canadashcb@gmail.com>
2025-07-17 15:37:18 +0200

    en cour

--- front/hackatweet_front/components/Login.js
+++ front/hackatweet_front/components/Login.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles/Login.module.css'
+import Signin from './Signin';
 
 const Login = () => {
-
+    const [signin, setSignin] = useState(true)
 
 
     return (
@@ -10,7 +11,7 @@ const Login = () => {
             <div className={styles.containerTexts}>
                 <p style={{fontSize: "8vmin", fontWeight:"900"}}>See what's happening</p>
                 <p style={{fontSize: "5vmin", fontWeight:"700"}}>Join Hackatweet today.</p>
-                <button className={styles.button} >Sign up</button>
+                {signin ? <Signin/> : <button className={styles.button} >Sign up</button>}
                 <p>Already habe on account?</p>
                 <button className={styles.button} >Sign in</button>
 
